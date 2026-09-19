d70fa41a1f1246be18dd03ae045726ca9d453812
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2026-09-19 16:12:05 +0530

    created simple 2 backends with simple starting routes

--- apps/http-server/src/app.ts
+++ apps/http-server/src/app.ts
@@ -1,7 +1,11 @@
 import express, { Request, Response, type Express } from "express";
 import cors from "cors";
+import bcrypt from "bcrypt";
+import { prisma } from "@repo/db";
+import jwt from "jsonwebtoken";
 
 const app: Express = express();
+const JWT_SECRET = process.env.JWT_SECRET || "pass";
 
 app.use(express.json());
 app.use(
@@ -16,6 +20,83 @@ app.get("/", (_req: Request, res: Response) => {
 
 app.get("/health", (_req: Request, res: Response) => {
   res.send("OK");
+});
+
+app.post("/auth/signup", async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: "Email and password are required" });
+  }
+
+  const isExists = await prisma.user.findFirst({
+    where: { email },
+  });
+
+  if (isExists) {
+    return res.status(400).json({
+      message: "User with this email is already Exists!",
+    });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await prisma.user.create({
+    data: {
+      email: email,
+      password: hashedPassword,
+    },
+  });
+
+  const token = await jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+    },
+    JWT_SECRET,
+  );
+
+  res.status(200).json({
+    message: "Signed Up Successfully!",
+    user,
+    token,
+  });
+});
+
+app.post("/auth/signin", async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  const user = await prisma.user.findFirst({
+    where: { email },
+  });
+
+  if (!user) {
+    return res
+      .status(404)
+      .json({ message: "User with this email dones not exists!" });
+  }
+
+  const comparePassword = await bcrypt.compare(password, user!.password);
+
+  if (!comparePassword) {
+    res.status(404).json({
+      message: "Invalid Credentials!",
+    });
+  }
+
+  const token = await jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+    },
+    JWT_SECRET,
+  );
+
+  res.status(200).json({
+    message: "Signed In Successfully!",
+    user: user,
+    token,
+  });
 });
 
 export default app;
--- apps/ws-server/src/app.ts
+++ apps/ws-server/src/app.ts
@@ -2,6 +2,12 @@ import { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
+wss.on("listening", () => console.log("Listining..."));
+
 wss.on("connection", (socket) => {
   console.log("New client connected");
+  socket.on("message", (data: string) => {
+    const parsedData = JSON.parse(data.toString());
+    console.log(parsedData);
+  });
 });
